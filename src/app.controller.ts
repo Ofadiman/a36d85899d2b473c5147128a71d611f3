ec67aa34f508ff71e8a32970ffe4074ed4f1964f
ofadiman <ofadiman@gmail.com>
2021-01-27 21:55:13 +0100

    feat: add health-check route (#13)
    - Change application port from hardcoded to dynamic provided by config service.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,9 @@
 import { Controller, Get } from '@nestjs/common'
 
-import { AppService } from './app.service'
-
 @Controller()
 export class AppController {
-  public constructor(private readonly appService: AppService) {}
-
-  @Get()
-  public getHello(): string {
-    return this.appService.getHello()
+  @Get('/health-check')
+  public healthCheck(): string {
+    return 'The app is running!'
   }
 }
