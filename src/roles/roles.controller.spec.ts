7cfd614f5b035264b31c6ebb468185f742ea0e78
ofadiman <ofadiman@gmail.com>
2021-01-29 11:59:03 +0100

    fix: failing tests in roles and controllers modules (#21)
    - Fix codegen template for controller spec.

--- src/roles/roles.controller.spec.ts
+++ src/roles/roles.controller.spec.ts
@@ -1,13 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing'
 
 import { RolesController } from './roles.controller'
+import { RolesService } from './roles.service'
 
 describe('RolesController', () => {
   let rolesController: RolesController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [RolesController]
+      controllers: [RolesController],
+      providers: [{ provide: RolesService, useValue: {} }]
     }).compile()
 
     rolesController = module.get<RolesController>(RolesController)
