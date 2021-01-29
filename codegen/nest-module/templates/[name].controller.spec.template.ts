40ad3f84d0a52fe9ef53beb7e25f1ec54fc4e3bc
ofadiman <ofadiman@gmail.com>
2021-01-29 11:59:03 +0100

    fix: failing tests in roles and controllers modules (#21)
    - Fix codegen template for controller spec.

--- codegen/nest-module/templates/[name].controller.spec.template.ts
+++ codegen/nest-module/templates/[name].controller.spec.template.ts
@@ -3,13 +3,15 @@ import { nestModuleConst } from '../nest-module.const'
 export const template = `import { Test, TestingModule } from '@nestjs/testing'
 
 import { ${nestModuleConst.plop.name.pascalCase}Controller } from './${nestModuleConst.plop.name.kebabCase}.controller'
+import { ${nestModuleConst.plop.name.pascalCase}Service } from './${nestModuleConst.plop.name.kebabCase}.service'
 
 describe('${nestModuleConst.plop.name.pascalCase}Controller', () => {
   let ${nestModuleConst.plop.name.camelCase}Controller: ${nestModuleConst.plop.name.pascalCase}Controller
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [${nestModuleConst.plop.name.pascalCase}Controller]
+      controllers: [${nestModuleConst.plop.name.pascalCase}Controller],
+      providers: [{ provide: ${nestModuleConst.plop.name.pascalCase}Service, useValue: {} }]
     }).compile()
 
     ${nestModuleConst.plop.name.camelCase}Controller = module.get<${nestModuleConst.plop.name.pascalCase}Controller>(${nestModuleConst.plop.name.pascalCase}Controller)
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,13 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing'
 
 import { UsersController } from './users.controller'
+import { UsersService } from './users.service'
 
 describe('UsersController', () => {
   let usersController: UsersController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController]
+      controllers: [UsersController],
+      providers: [{ provide: UsersService, useValue: {} }]
     }).compile()
 
     usersController = module.get<UsersController>(UsersController)
